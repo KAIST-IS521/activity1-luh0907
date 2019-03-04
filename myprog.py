37c0bea1e753a36817d4bbc8a4fa96cb8e5d4fec
luh0907 <luh0907@gmail.com>
2019-03-04 16:47:56 +0900

    added basic exception controls

--- myprog.py
+++ myprog.py
@@ -2,16 +2,30 @@ import sys
 import csv
 
 def checkWellFormedCSV(f):
-    pass
+    # Ref:
+    # https://stackoverflow.com/questions/2984888/check-if-file-has-a-csv-format-with-python
+    try:
+        dialect = csv.Sniffer().sniff(f.read())
+        f.seek(0)
+    except csv.Error:
+        exit(1)
 
 def parseCSV(f):
-    pass
+   pass 
 
 if __name__ == "__main__":
+    if not len(sys.argv) == 3:
+        print("Usage: python myprog.py [csvfile] [column #]\n")
+        exit(1)
+
     filename = sys.argv[1]
     N = sys.argv[2]
 
-    f = open(filename, 'r')
+    try:
+        f = open(filename, 'r')
+    except:
+        print("Cannot open given input file.\n")
+        exit(1)
 
     checkWellFormedCSV(f)
     parseCSV(f)
